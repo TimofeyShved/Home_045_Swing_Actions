7c3e176d5cd7b6f9b630fbf95be8aaba51faee1c
timofeya123 <timofeya123@inbox.ru>
2021-03-02 20:53:30 +0600

    KeyStroke

--- src/com/Swing/Main.java
+++ src/com/Swing/Main.java
@@ -5,17 +5,33 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 
 public class Main {
-
+    // Action
     static JFrame jFrame = getFrame(); // наша форма
     static JPanel jPanel = new JPanel(); // наша панель
 
     public static void main(String[] args) {
+        // Action
         jFrame.add(jPanel); // добавляем панель
         JButton jButton = new JButton(new MyAction()); // создаем кнопку с действием
         jButton.setText("Hello");
         jPanel.add(jButton);
+
+        // Action + KeyStroke
+        AbstractAction myAction = new MyAction(); // наше действие
+        KeyStroke keyStroke = KeyStroke.getKeyStroke("ctrl B"); // сочетание клавиш
+
+        // список ключей и их наименований доставаемых из панели
+        //когда в фокусе определённый элемент JComponent.WHEN_FOCUSED,
+        // когда в фокусе окно JComponent.WHEN_IN_FOCUSED_WINDOW
+        InputMap inputMap = jPanel.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        inputMap.put(keyStroke, "changeColor");
+
+        // список действий
+        ActionMap actionMap = jPanel.getActionMap();
+        actionMap.put("changeColor", myAction); // действие соответвующей названию ключа
     }
 
+    // Action
     static class MyAction extends AbstractAction{
 
         MyAction(){ // Действие при наведении, подсказки
@@ -28,6 +44,7 @@ public class Main {
         }
     }
 
+    // Action
     static JFrame getFrame(){
         JFrame jFrame = new JFrame(); // создается форма
         jFrame.setVisible(true); // влючаем видимость
